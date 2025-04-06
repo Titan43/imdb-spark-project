5ded3cf66acf1a6ff9dd164afd97997c204026e2
Serhii Hrynevych <serhiy@getsomatic.com>
2025-04-06 22:15:52 +0300

    LARGE REFACTORING INCOMING

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from pyspark.sql import SparkSession
 from questions.df_preload import preload_dfs
+from questions import write_output
 
 spark = SparkSession.builder.appName("DataProcessingApp").getOrCreate()
 df_s = preload_dfs(spark, 
                    ["https://datasets.imdbws.com/title.basics.tsv.gz",
                     "https://datasets.imdbws.com/title.crew.tsv.gz",
                     "https://datasets.imdbws.com/name.basics.tsv.gz"])
+
+write_output.run(df_s)
