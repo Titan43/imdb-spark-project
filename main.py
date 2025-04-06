89a110ed998ed743fa77707baf2642609495f9b5
Serhii Hrynevych <serhiy@getsomatic.com>
2025-04-06 20:41:59 +0300

    refactor and paralelize data reading

--- main.py
+++ main.py
@@ -1,19 +1,8 @@
 from pyspark.sql import SparkSession
-from utils.data_loader import DataLoader 
-from utils.data_writer import DataWriter 
+from questions.df_preload import preload_dfs
 
 spark = SparkSession.builder.appName("DataProcessingApp").getOrCreate()
-
-CACHE_PATH = "/app/.cache"
-
-data_loader = DataLoader(spark, CACHE_PATH)
-data_writer = DataWriter(CACHE_PATH)
-
-url = "https://datasets.imdbws.com/name.basics.tsv.gz"
-df = data_loader.load_data(url)
-
-if df is not None:
-    df.show()
-    data_writer.save_as_csv(df, "test")
-else:
-    print("Failed to load data.")
+df_s = preload_dfs(spark, 
+                   ["https://datasets.imdbws.com/title.basics.tsv.gz",
+                    "https://datasets.imdbws.com/title.crew.tsv.gz",
+                    "https://datasets.imdbws.com/name.basics.tsv.gz"])
