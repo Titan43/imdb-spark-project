344e64d215eb7135119c4e34a0bba64030ddade1
Serhii Hrynevych <serhiy@getsomatic.com>
2025-04-06 19:38:31 +0300

    add data writer

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 from pyspark.sql import SparkSession
-from utils.data_loader import DataLoader  
+from utils.data_loader import DataLoader 
+from utils.data_writer import DataWriter 
 
 spark = SparkSession.builder.appName("DataProcessingApp").getOrCreate()
 
-data_loader = DataLoader(spark, "/app/.cache")
+CACHE_PATH = "/app/.cache"
+
+data_loader = DataLoader(spark, CACHE_PATH)
+data_writer = DataWriter(CACHE_PATH)
 
 url = "https://datasets.imdbws.com/name.basics.tsv.gz"
 df = data_loader.load_data(url)
 
 if df is not None:
     df.show()
+    data_writer.save_as_csv(df, "test")
 else:
     print("Failed to load data.")
