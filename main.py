35234896fc52bcd8f6e39158f392ded36cea29a5
Serhii Hrynevych <serhiy@getsomatic.com>
2025-04-07 03:01:08 +0300

    refactor to support new question format

--- main.py
+++ main.py
@@ -7,7 +7,10 @@ def main():
     df_s = preload_dfs(spark, 
                     ["https://datasets.imdbws.com/title.basics.tsv.gz",
                         "https://datasets.imdbws.com/title.crew.tsv.gz",
-                        "https://datasets.imdbws.com/name.basics.tsv.gz"])
+                        "https://datasets.imdbws.com/name.basics.tsv.gz",
+                        "https://datasets.imdbws.com/title.principals.tsv.gz",
+                        "https://datasets.imdbws.com/title.ratings.tsv.gz",
+                        "https://datasets.imdbws.com/title.episode.tsv.gz"])
 
     get_question_results.run(df_s)
 
--- questions/get_question_results.py
+++ questions/get_question_results.py
@@ -10,15 +10,29 @@ from .questions1 import (
     get_most_common_genres_by_top_directors,
     get_top_most_consistent_directors
 )
+from .questions2 import (
+    get_comedies_after_2010,
+    get_top10_rated,
+    get_drama_directors,
+    get_breaking_bad_episodes,
+    get_actors_with_most_series,
+    get_top10_genres_by_film_count
+)
 from . import CACHE_PATH
 
 questions_implementation = {
-    "get_genres_by_avg_runtime":get_genres_by_avg_runtime,
-    "get_genres_by_dominating_long_running_series":get_genres_by_dominating_long_running_series,
-    "get_years_with_most_releases":get_years_with_most_releases,
-    "get_directors_with_most_movies":get_directors_with_most_movies,
-    "get_most_common_genres_by_top_directors":get_most_common_genres_by_top_directors,
-    "get_top_most_consistent_directors":get_top_most_consistent_directors,
+    "genres_by_avg_runtime":get_genres_by_avg_runtime,
+    "genres_by_dominating_long_running_series":get_genres_by_dominating_long_running_series,
+    "years_with_most_releases":get_years_with_most_releases,
+    "directors_with_most_movies":get_directors_with_most_movies,
+    "most_common_genres_by_top_directors":get_most_common_genres_by_top_directors,
+    "top_most_consistent_directors":get_top_most_consistent_directors,
+    "comedies_after_2010":get_comedies_after_2010,
+    "top10_rated":get_top10_rated,
+    "drama_directors":get_drama_directors,
+    "breaking_bad_episodes":get_breaking_bad_episodes,
+    "actors_with_most_series":get_actors_with_most_series,
+    "top10_genres_by_film_count":get_top10_genres_by_film_count
 }
 
 def run(df_s: Dict[str, DataFrame]):
